5d04f8d2b663a6c009ee3f1bfd84b19d5479d016
Alex Curci <alex.curci@gmail.com>
2019-08-25 14:38:11 -0400

    Seed test user, createUser and createExcuse mutation

--- server/src/typeDefs.js
+++ server/src/typeDefs.js
@@ -5,12 +5,32 @@ const typeDefs = gql`
     id: Int!
     category: String!
     caption: String!
+    userId: Int
+  }
+  type User {
+    id: Int!
+    userName: String!
+    email: String!
+    password: String!
   }
   type Query {
     "A simple type for getting started!"
     hello: String
     excuse(id: ID!): Excuse
     excuses: [Excuse!]!
+  }
+  type Mutation {
+    createExcuse(
+      category: String!
+      caption: String!
+      userId: Int
+    ): Excuse!
+    createUser(
+      userName: String!
+      email: String!
+      password: String!
+    ): User!
+
   }
 
 `;
