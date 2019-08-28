2826d7fabf09c0cbbf2224bcd576fc81a0c10fe2
Alex Curci <alex.curci@gmail.com>
2019-08-27 23:43:27 -0400

    Add updateExcuse mutation reosolver and typedef

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -9,6 +9,25 @@ const resolvers = {
     async createExcuse (root, { category, caption, userId }, { models }) {
       return models.Excuse.create({ category, caption, userId })
     },
+    async updateExcuse (root, { id, category, caption, userId }, { models }) {
+      const excuseObj = {};
+
+      if (caption !== undefined) {
+        excuseObj.caption = caption; 
+      }
+      if (category !== undefined) {
+        excuseObj.category = category; 
+      }
+      if (userId !== undefined) {
+        excuseObj.userId = userId;
+      }
+
+      models.Excuse.update( excuseObj, {
+        where: { id: id }    
+      });
+
+      return excuseObj;
+    },
     async createUser (root, { userName, email, password }, { models }) {
       return models.User.create({ userName, email, password })
     }
